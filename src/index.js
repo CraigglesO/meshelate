e07789ee55a60118df9a85ac1f91887d3efdbc63
CraigglesO <oconnorct1@gmail.com>
2020-03-29 23:24:54 -0400

    add support for index offsets

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 For each polygon, if the line is too long, split at the mid-point with the opposite corner
 
 **/
-function meshelate (vertices, srcIndices, maxLength = Infinity, dim = 2) {
+function meshelate (vertices, srcIndices, maxLength = Infinity, offset = 0, dim = 2) {
   const il = srcIndices.length
   const indices = []
 
@@ -21,10 +21,10 @@ function meshelate (vertices, srcIndices, maxLength = Infinity, dim = 2) {
         bI = (j + 1) % 3
         b = stack[bI]
         // if line too long, split and start over
-        if (isTooLong(a, b, vertices, maxLength, dim)) {
+        if (isTooLong(a, b, vertices, maxLength, offset, dim)) {
           cI = (j + 2) % 3
           c = stack[cI]
-          for (let k = 0; k < dim; k++) vertices.push((vertices[b * dim + k] + vertices[a * dim + k]) / 2)
+          for (let k = 0; k < dim; k++) vertices.push((vertices[(b - offset) * dim + k] + vertices[(a - offset) * dim + k]) / 2)
           // split
           vl = (vertices.length - 2) / dim
           // during split, we also rotate all by 1, so if the result was a-b-c, we would do c-a-b
@@ -48,12 +48,12 @@ function meshelate (vertices, srcIndices, maxLength = Infinity, dim = 2) {
   return { vertices, indices }
 }
 
-function isTooLong (a, b, vertices, maxLength, dim) {
+function isTooLong (a, b, vertices, maxLength, offset, dim) {
   let length = 0
   let diff
 
   for (let i = 0; i < dim; i++) {
-    diff = vertices[b * dim + i] - vertices[a * dim + i]
+    diff = vertices[(b - offset) * dim + i] - vertices[(a - offset) * dim + i]
     length += diff * diff
   }
 
